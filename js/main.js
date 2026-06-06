8ec4ab0ea53145b589f04b44aa3b64a903c2bec5
Limbert-Tudela <eate.limbertroly.tudela.ma@unifranz.edu.bo>
2026-06-06 12:21:50 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function getToken() {
+    console.log("Demo: getToken() llamado - El sistema completo requiere Node.js");
+    return null;
+}
+
+function getUser() {
+    console.log("Demo: getUser() llamado - El sistema completo requiere Node.js");
+    return null;
+}
+
+function setAuth(token, user) {
+    console.log("Demo: setAuth() llamado - El sistema completo requiere Node.js");
+}
+
+function logout() {
+    console.log("Demo: Cerrando sesión (simulado)");
+    window.location.href = '/index.html';
+}
+
+console.log("DONA-CHAIN - Versión demostrativa. El sistema completo con blockchain requiere servidor Node.js.");
